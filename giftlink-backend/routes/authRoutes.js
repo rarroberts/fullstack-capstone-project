13b0b9691310298946e2c5f3a716d52ede059369
rarroberts <rlt.tcr@gmail.com>
2026-09-18 22:46:12 -0400

    Registration

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -13,6 +13,8 @@ const logger = pino();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
+
+// REGISTER
 router.post('/register', async (req, res) => {
     try {
         // Task 1: Connect to MongoDB
@@ -46,18 +48,90 @@ router.post('/register', async (req, res) => {
         // Task 5: Create JWT authentication
         const payload = {
             user: {
-                id: newUser.insertedId,
+                id: newUser.insertedId.toString(),
             },
         };
 
         const authtoken = jwt.sign(payload, JWT_SECRET);
 
         logger.info('User registered successfully');
-        res.json({ authtoken, email });
+
+        return res.json({
+            authtoken,
+            email
+        });
 
     } catch (e) {
+        logger.error(e);
         return res.status(500).send('Internal server error');
     }
 });
+
+
+// LOGIN
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to giftsdb through connectToDatabase
+        const db = await connectToDatabase();
+
+        // Task 2: Access users collection
+        const collection = db.collection("users");
+
+        // Task 3: Check for user credentials in database
+        const theUser = await collection.findOne({
+            email: req.body.email
+        });
+
+        if (theUser) {
+
+            // Task 4: Check if password matches encrypted password
+            const result = await bcryptjs.compare(
+                req.body.password,
+                theUser.password
+            );
+
+            if (!result) {
+                logger.error('Passwords do not match');
+
+                return res.status(404).json({
+                    error: 'Wrong pasword'
+                });
+            }
+
+            // Task 5: Fetch user details
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+
+            // Task 6: Create JWT authentication
+            const payload = {
+                user: {
+                    id: theUser._id.toString(),
+                },
+            };
+
+            const authtoken = jwt.sign(payload, JWT_SECRET);
+
+            return res.json({
+                authtoken,
+                userName,
+                userEmail
+            });
+
+        } else {
+
+            // Task 7: User not found
+            logger.error('User not found');
+
+            return res.status(404).json({
+                error: 'User not found'
+            });
+        }
+
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).send('Internal server error');
+    }
+});
+
 
 module.exports = router;
