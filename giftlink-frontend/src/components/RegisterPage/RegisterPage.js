dcca09018b1d43798d7fe786c6711e895bc34504
rarroberts <rlt.tcr@gmail.com>
2026-09-19 10:59:46 -0400

    Fix JSHint ES8 configuration

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
 import './RegisterPage.css';
-import { urlConfig } from '../config';
-import { useAppContext } from '../context/AuthContext';
+import { urlConfig } from '../../config';
+import { useAppContext } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
 function RegisterPage() {
